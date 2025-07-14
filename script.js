94ba9ffeec9a82176e8f5a1e002a13b214873476
Harivansh Mehta <harivanshmehta4@gmail.com>
2025-07-14 12:44:11 +0530

    Organize divs into cells and rows

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 divContainer = document.querySelector(".grid-container");
+let grid = [[], [], [], []];
 for (let i = 0; i < 16; i++) {
     const newDiv = document.createElement('div')
     newDiv.textContent = `${i+1}`;
     newDiv.classList.add('cell');
     newDiv.id = `cell-${i+1}`;
-    divContainer.appendChild(newDiv);
+    grid[Math.floor(i/4)].push(newDiv);
+}
+for (let i = 0; i < 4; i++) {
+    const newRow = document.createElement('div');
+    newRow.classList.add('row');
+    newRow.id = `row-${i+1}`;
+    for (cell of grid[i]) {
+        newRow.appendChild(cell);
+    }
+    divContainer.appendChild(newRow);
 }
