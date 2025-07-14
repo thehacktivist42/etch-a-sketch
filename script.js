8f74ca9e93f0a231941fef0eb725af2af6d2822a
Harivansh Mehta <harivanshmehta4@gmail.com>
2025-07-14 13:05:36 +0530

    Add hover functionality + bug fixes

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 divContainer = document.querySelector(".grid-container");
-let grid = [[], [], [], []];
+let grid = [];
 for (let i = 0; i < 16; i++) {
+    grid.push([]);
+}
+
+for (let i = 0; i < 256; i++) {
     const newDiv = document.createElement('div')
     newDiv.classList.add('cell');
     newDiv.id = `cell-${i+1}`;
+    newDiv.addEventListener('mouseenter', () => hovered(newDiv))
     grid[Math.floor(i/4)].push(newDiv);
 }
+
 for (let i = 0; i < 4; i++) {
     const newRow = document.createElement('div');
     newRow.classList.add('row');
@@ -14,4 +20,8 @@ for (let i = 0; i < 4; i++) {
         newRow.appendChild(cell);
     }
     divContainer.appendChild(newRow);
+}
+
+function hovered(cell) {
+    cell.classList.add('hovered');
 }
